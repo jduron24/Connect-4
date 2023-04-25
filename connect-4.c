5bbac8ef55d84f3ee7b7beea178871a065de1bc9
jduron24 <jduron24@iastate.edu>
2023-04-25 17:20:00 -0500

    game Board and ncurses added

--- connect-4.c
+++ connect-4.c
@@ -1,12 +1,85 @@
 #include <stdio.h>
 #include <ncurses.h>
 
+void game_board()
+{
+    clear();
+    mvprintw(4, 15,
+             "|      |       |       |       |       |       |      |\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "+------+-------+-------+-------+-------+-------+------|\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "+------+-------+-------+-------+-------+-------+------|\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "+------+-------+-------+-------+-------+-------+------|\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "+------+-------+-------+-------+-------+-------+------|\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "+------+-------+-------+-------+-------+-------+------|\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "|      |       |       |       |       |       |      |\n               "
+             "+------+-------+-------+-------+-------+-------+------|\n               "
+              
+              
+              );
+}
 
 
-int main(int argc, char* argv[]){
-
-printf("hello world");
 
 
-return 0;
+
+void init_curses()
+{
+    int ch;
+    int to;
+    initscr();
+    keypad(stdscr, TRUE);
+    mvprintw(0, 35, "Connect 4");
+    mvprintw(10, 35, "Start game");
+    mvprintw(12, 35, "Credits");
+    mvprintw(10, 34, ">");
+
+    while (ch != KEY_LEFT)
+    {
+        ch = getch();
+        if (ch == KEY_UP)
+        {
+            clear();
+            mvprintw(0, 35, "Connect 4");
+            mvprintw(10, 35, "Start game");
+            mvprintw(12, 35, "Credits");
+
+            mvprintw(10, 34, ">");
+            to = 1;
+            refresh();
+        }
+        else if (ch == KEY_DOWN)
+        {
+            clear();
+            mvprintw(0, 35, "Connect 4");
+            mvprintw(10, 35, "Start game");
+            mvprintw(12, 35, "Credits");
+
+            mvprintw(12, 34, ">");
+            to = 0;
+            refresh();
+        }
+    }
+    if(to == 1){
+        game_board();
+    }
+
+    endwin();
+}
+
+int main(int argc, char *argv[])
+{
+
+    init_curses();
+
+    return 0;
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 connect-4: connect-4.c
-	gcc -o connect-4 
+	gcc -o connect-4 connect-4.c -lncurses
 run:
-	./connect-4 
+	./connect-4
 clean:
 	rm -rf connect-4
